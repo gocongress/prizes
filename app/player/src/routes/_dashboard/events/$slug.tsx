19547d32395cd9803df202d3161fc11b67eccffc
Josh Gum <revgum@gmail.com>
2026-01-17 07:30:48 -0800

    uses the event sponsors page when logged in as well

--- app/player/src/routes/_dashboard/events/$slug.tsx
+++ app/player/src/routes/_dashboard/events/$slug.tsx
@@ -1,4 +1,4 @@
-import EventPage from '@/components/pages/event-page';
+import EventSponsorsPage from '@/components/pages/event-sponsors-page';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_dashboard/events/$slug')({
@@ -6,5 +6,5 @@ export const Route = createFileRoute('/_dashboard/events/$slug')({
 });
 
 function RouteComponent() {
-  return <EventPage breadcrumbs={[{ label: 'Upcoming Events', href: '/events' }]} />;
+  return <EventSponsorsPage breadcrumbs={[{ label: 'Upcoming Events', href: '/events' }]} />;
 }
