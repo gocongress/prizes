3034690861cadc7a0196efc415a67003b9113ab9
Josh Gum <revgum@gmail.com>
2026-07-02 11:01:46 -0700

    fixes player app build

--- app/player/vite.config.ts
+++ app/player/vite.config.ts
@@ -5,7 +5,8 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig((env) => {
+// export default defineConfig((env) => {
+export default defineConfig(() => {
   return {
     plugins: [
       tanstackRouter({
