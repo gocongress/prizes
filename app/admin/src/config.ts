f961a1c52eaf4f1b434b8b5814357bae09b5e230
Josh Gum <revgum@gmail.com>
2026-07-06 08:01:23 -0700

    export 10k max records

--- app/admin/src/config.ts
+++ app/admin/src/config.ts
@@ -2,7 +2,7 @@
 export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
 export const OTP_CODE_LENGTH = Number.parseInt(import.meta.env.VITE_OTP_CODE_LENGTH || '8');
 export const TURNSTILE_SITE_KEY = import.meta.env.VITE_TURNSTILE_SITE_KEY;
-export const EXPORT_MAX_RESULTS = 100_000;
+export const EXPORT_MAX_RESULTS = 10_000;
 
 // Constants NOT included in the build
 export const APP_BASENAME = import.meta.env.APP_BASENAME || '/';
